f3c7b2fac46b96cfe831c5e19306b526a7157241
kresno46 <kresnonugroho46@gmail.com>
2026-02-21 23:43:15 +0700

    udpate:beranda

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -269,54 +269,6 @@
             @endforelse
         </div>
     </div>
-    <!-- Award -->
-    <div id="Awards" class="reveal container max-w-[1130px] mx-auto flex flex-col gap-[30px] mt-20">
-        <div class="flex items-center justify-between">
-            <div class="flex flex-col gap-[14px]">
-                <p class="badge w-fit bg-cp-pale-blue text-cp-light-blue p-[8px_16px] rounded-full uppercase font-bold text-sm">PENGHARGAAN KAMI</p>
-                <h2 class="font-bold text-4xl leading-[45px]">Kami Mendedikasikan<br />Upaya Terbaik untuk Kayu Berkualitas</h2>
-            </div>
-            <a href="" class="bg-cp-black p-[14px_20px] w-fit rounded-xl font-bold text-white">Jelajahi Lebih Banyak</a>
-        </div>
-        <div class="awards-card-container grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-[30px] justify-center">
-            <div class="card bg-white flex flex-col h-full p-[30px] gap-[30px] rounded-[20px] border border-[#E8EAF2] hover:border-cp-dark-blue transition-all duration-300">
-                <div class="w-[55px] h-[55px] flex shrink-0">
-                    <img src="{{ asset('assets/icons/cup-blue.svg') }}" alt="icon" />
-                </div>
-                <hr class="border-[#E8EAF2]" />
-                <p class="font-bold text-xl leading-[30px]">Fondasi Solid dan Pengerjaan Rapi</p>
-                <hr class="border-[#E8EAF2]" />
-                <p class="text-cp-light-grey">Bali, 2020</p>
-            </div>
-            <div class="card bg-white flex flex-col h-full p-[30px] gap-[30px] rounded-[20px] border border-[#E8EAF2] hover:border-cp-dark-blue transition-all duration-300">
-                <div class="w-[55px] h-[55px] flex shrink-0">
-                    <img src="{{ asset('assets/icons/cup-blue.svg') }}" alt="icon" />
-                </div>
-                <hr class="border-[#E8EAF2]" />
-                <p class="font-bold text-xl leading-[30px]">Tempat Tersibuk Namun Tetap Harmonis</p>
-                <hr class="border-[#E8EAF2]" />
-                <p class="text-cp-light-grey">Shanghai, 2021</p>
-            </div>
-            <div class="card bg-white flex flex-col h-full p-[30px] gap-[30px] rounded-[20px] border border-[#E8EAF2] hover:border-cp-dark-blue transition-all duration-300">
-                <div class="w-[55px] h-[55px] flex shrink-0">
-                    <img src="{{ asset('assets/icons/cup-blue.svg') }}" alt="icon" />
-                </div>
-                <hr class="border-[#E8EAF2]" />
-                <p class="font-bold text-xl leading-[30px]">Hal Kecil Memberi Dampak Besar</p>
-                <hr class="border-[#E8EAF2]" />
-                <p class="text-cp-light-grey">Zurich, 2022</p>
-            </div>
-            <div class="card bg-white flex flex-col h-full p-[30px] gap-[30px] rounded-[20px] border border-[#E8EAF2] hover:border-cp-dark-blue transition-all duration-300">
-                <div class="w-[55px] h-[55px] flex shrink-0">
-                    <img src="{{ asset('assets/icons/cup-blue.svg') }}" alt="icon" />
-                </div>
-                <hr class="border-[#E8EAF2]" />
-                <p class="font-bold text-xl leading-[30px]">Kerja Sama dan Solidaritas</p>
-                <hr class="border-[#E8EAF2]" />
-                <p class="text-cp-light-grey">Bandung, 2023</p>
-            </div>
-        </div>
-    </div>
     <!-- FAQ -->
     <div id="FAQ" class="reveal bg-[#F6F7FA] w-full py-20 px-[10px] mt-20 -mb-20">
         <div class="container max-w-[1000px] mx-auto">
